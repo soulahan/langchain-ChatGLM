54473ace30ed33c003268b29aa5d605abd64a6ff
sch <baiyunlantian86@gmail.com>
2023-07-06 16:30:10 +0800

    接入openai

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .chatglm_llm import ChatGLM
+from .llama_llm import LLamaLLM
+from .moss_llm import MOSSLLM
+from .fastchat_openai_llm import FastChatOpenAILLM
+from .openai_llm import OpenAILLM
